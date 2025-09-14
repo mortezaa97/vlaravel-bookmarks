a4a2840839ff5232662a374fdbc213734d22f9f9
mortezaa <mortezajafari76@gmail.com>
2025-09-15 01:04:02 +0330

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,10 @@
 
 declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
-use Mortezaa97\Sliders\Http\Controllers\SlideController;
-use Mortezaa97\Sliders\Http\Controllers\SliderController;
-use Mortezaa97\Stories\Http\Controllers\StoryController;
+use Mortezaa97\Bookmarks\Http\Controllers\BookmarkController;
 
-Route::prefix('api/sliders')->group(function () {
-    Route::get('/', [SliderController::class, 'index'])->name('sliders.index');
-    Route::get('/{slider}', [SliderController::class, 'show'])->name('sliders.show');
-});
-Route::prefix('api/slides')->group(function () {
-    Route::get('/', [SlideController::class, 'index'])->name('slides.index');
-    Route::get('/{slide}', [SlideController::class, 'show'])->name('slides.show');
+Route::prefix('api/bookmarks')->group(function () {
+    Route::get('/', [BookmarkController::class, 'index'])->name('bookmarks.index');
+    Route::get('/{bookmark}', [BookmarkController::class, 'show'])->name('bookmarks.show');
+
 });
