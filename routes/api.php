66c5b44c07ff981fac3883dd813a1cdbb8087a0a
mortezaa <mortezajafari76@gmail.com>
2025-09-27 13:13:37 +0330

    fix:route api middleware issue

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Mortezaa97\Bookmarks\Http\Controllers\BookmarkController;
 
-Route::prefix('api/bookmarks')->group(function () {
+Route::prefix('api/bookmarks')->middleware('api')->group(function () {
     Route::get('/', [BookmarkController::class, 'index'])->name('bookmarks.index');
     Route::get('/{bookmark}', [BookmarkController::class, 'show'])->name('bookmarks.show');
 
